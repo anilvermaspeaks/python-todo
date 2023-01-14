0df1b2455aa6f07da6ffbae6f6c5fba678f25a1f
Anil Verma <anilcs240@gmail.com>
2023-01-14 16:46:54 +0530

    added type conversion

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 todos = []
 while True:
-    user_action = input("Type action(add or show): ")
+    user_action = input("Type action(add,show or edit): ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
+        case 'edit':
+            for item in todos:
+                print(item)
+            number = int(input("index no of todo to change: "))
+            number = number -1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case 'show':
             for item in todos:
                 print(item)
